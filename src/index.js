afd11e7412e5730ad9c0ae7b8d863e987a329f00
Ragesh-A <ragesha2017@gmail.com>
2023-05-17 14:47:57 +0530

    initial server setup

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const logger = require('morgan')
+require('dotenv').config()
+
+const PORT = process.env.PORT || 8000
+
+const app = express()
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.listen(PORT,()=>{
+  console.log(`server running on port ${PORT}`)
+})
+
+
+

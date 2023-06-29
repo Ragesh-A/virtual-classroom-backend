cf917ad9c9820f46b8f049126876550ceadcaa87
Ragesh-A <ragesha2017@gmail.com>
2023-06-29 10:00:32 +0530

    .

--- src/api/utils/jwt.js
+++ src/api/utils/jwt.js
@@ -1,12 +1,16 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const jwt = require('jsonwebtoken');
 
-const generateToken = (payload) => jwt
-  .sign(payload, process.env.JWT_SECRET_KEY /* { expiresIn: '1h' */);
+const generateToken = (payload) => {
+  const SECRET_KEY = process.env.JWT_SECRET_KEY
+  return jwt
+  .sign(payload,  SECRET_KEY/* { expiresIn: '1h' */);
+}
 
 const verifyToken = async (token) => {
   try {
-    const decoded = await jwt.verify(token, process.env.JWT_SECRET_KEY);
+    const SECRET_KEY = process.env.JWT_SECRET_KEY
+    const decoded = await jwt.verify(token, SECRET_KEY);
     return decoded;
   } catch (error) {
     throw new Error('invalid access token');

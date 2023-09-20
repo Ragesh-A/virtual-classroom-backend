d83a45411c2b14d7df8d91ca597cd8d246f5f42a
Ragesh-A <ragesha2017@gmail.com>
2023-09-20 09:27:28 +0530

    meetup server

--- src/socket/meetup.server.js
+++ src/socket/meetup.server.js
@@ -3,46 +3,124 @@ require('dotenv').config();
 const { REACT_IP } = process.env;
 
 const io = require('socket.io')(9090, {
-  cors: {
-    origin: REACT_IP,
-  },
+	cors: {
+		origin: REACT_IP,
+	},
 });
 
-const rooms = {};
+// io.on('connection', (connection) => {
+// 	connection.on(
+// 		'create',
+// 		catchError((roomId) => {
+// 			const room = getRoom(io, roomId);
+// 			if (room) throw new Error('try again');
+// 			connection.join(roomId);
+// 			connection.emit('created', { success: 'created successfully' });
+// 		})
+// 	);
 
-const getOffer = (userId) => rooms[userId];
+// 	connection.on(
+// 		'join',
+// 		catchError((roomId, user) => {
+// 			console.log('joined', roomId);
+// 			if (!user || !roomId) throw new Error('invalid data' + roomId + user);
+// 			const room = getRoom(io, roomId);
+// 			if (!room) throw new Error('No such room');
+// 			if (room.size > 0 && room.size < 50) {
+// 				connection.join(roomId);
+// 				connection.emit('joined');
+// 				connection.broadcast.to(roomId).emit('new-joiner', user);
+// 			} else {
+// 				connection.emit('full');
+// 			}
+// 		})
+// 	);
 
+// 	connection.on(
+// 		'ready',
+// 		catchError((roomId) => {
+// 			if (!roomId) throw new Error('invalid data' + roomId);
+// 			const room = getRoom(io, roomId);
+// 			if (!room) throw new Error('No such room');
+// 			console.log('ready', roomId);
+// 			connection.broadcast.to(roomId).emit('ready');
+// 		})
+// 	);
 
-io.on('connection', (connection) => {
-  console.log(connection.id);
+// 	connection.on(
+// 		'candidate',
+// 		catchError((candidate, roomId, userId) => {
+// 			if (!candidate || !roomId)
+// 				throw new Error('invalid data' + roomId + candidate);
+// 			const room = getRoom(io, roomId);
+// 			if (!room) throw new Error('No such room');
+// 			console.log('candiadate', candidate);
+// 			connection.broadcast.to(roomId).emit('candidate', candidate, userId);
+// 		})
+// 	);
 
-  connection.on('login', (data) => {
-    if (!data.user) return;
-    rooms[data?.user] = {
-      userId: data?.user,
-      socketId: connection.id,
-    };
-    console.log(rooms);
-  });
+// 	connection.on(
+// 		'offer',
+// 		catchError((offer, roomId) => {
+// 			if (!roomId || !offer) throw new Error('Invalid data');
+// 			const room = getRoom(io, roomId);
+// 			if (!room) throw new Error('No such room');
+// 			console.log('offer ');
+// 			connection.broadcast.to(roomId).emit('offer', offer);
+// 		})
+// 	);
 
-  connection.on('create-new-room', (data) => {
-    try {
-      if (!data?.offer) return;
-      rooms[data?.user?.emailOrPhone].offer = data?.offer;
-      rooms[data?.user?.emailOrPhone].username = data?.user?.name;
-      console.log(rooms);
-    } catch (error) {
-      console.log(error);
-    }
-  });
+// 	connection.on(
+// 		'answer',
+// 		catchError((answer, roomId) => {
+// 			if (!answer || !roomId) throw new Error('Invalid data');
+// 			const room = getRoom(io, roomId);
+// 			if (!room) throw new Error('No such room');
+// 			console.log('answer');
+// 			connection.broadcast.to(roomId).emit('answer', answer);
+// 		})
+// 	);
 
-  connection.on('join-room', ({ user, roomId }) => {
-    if (!rooms[roomId]) return;
-    io.to(rooms[user?.emailOrPhone]).emit({ offer: rooms[roomId].offer });
-  });
+// 	function catchError(fn) {
+// 		return (...args) => {
+// 			try {
+// 				fn(...args);
+// 			} catch (error) {
+// 				connection.emit('error', error.message);
+// 			}
+// 		};
+// 	}
+// });
+// function getRoom(io, roomId) {
+// 	const rooms = io.sockets.adapter.rooms;
+// 	const room = rooms.get(roomId);
+// 	return room;
+// }
 
-  connection.on('disconnect', () => {
-    console.log('disconnected');
-    io.emit(connection.id);
-  });
+const participants = {}
+
+io.on('connection', socket => {
+	console.log('User connected');
+
+	socket.on('join', () => {
+		participants[socket.id] = socket;
+		socket.broadcast.emit('joined', socket.id);
+	});
+
+	socket.on('candidate', (candidate, targetParticipantId) => {
+		participants[targetParticipantId].emit('candidate', candidate. socket.id);
+	});
+
+	socket.on('offer', (offer, targetParticipantId) => {
+		participants[targetParticipantId].emit('offer',  offer, socket.id,);
+	});
+
+	socket.on('answer', (answer, targetParticipantId) => {
+		participants[targetParticipantId].emit('answer',  answer, socket.id);
+	});
+
+	socket.on('disconnect', () => {
+		console.log('User disconnected');
+		delete participants[socket.id];
+	});
 });
